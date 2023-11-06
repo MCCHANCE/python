d0e69fd1ac7c58eb332283386bdcbbcbd4422d88
MCCHANCE <unwananyong2021@gmail.com>
2023-11-06 12:32:21 -0800

    user_input1.py

---
+++ multiplication.py/user_input1.py
@@ -0,0 +1,5 @@
+# Exercise: ask questions; person's name and favourite colour. Then, print out a response on the terminal
+
+name = input(' what is your name? ')
+favourite_colour = input(' what is your favourite colour? ')
+print(name + ' likes ' + favourite_colour)
