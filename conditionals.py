0a6eb558f37c2e8f5cf2c0072d3a02ae9816372d
MCCHANCE <unwananyong2021@gmail.com>
2023-11-12 11:24:15 -0800

    compare.py

--- conditionals.py
+++ conditionals.py
@@ -1,4 +1,18 @@
+''''''
 condition = 10
 
 if condition:
     print('True')
+''''''
+
+# Prompt user for integers
+x = input("What's x? ")
+y = input("What's y? ")
+
+# Compare integers
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
