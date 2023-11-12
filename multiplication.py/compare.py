285f12e22400b7868efef6057dae36a5b3741dd7
MCCHANCE <unwananyong2021@gmail.com>
2023-11-12 11:17:09 -0800

    compare.py

---
+++ multiplication.py/compare.py
@@ -0,0 +1,9 @@
+#prompt the user to agree
+s = input('do you agree? ')
+
+#cheack whether agreed
+if s == "Y" or s == "y":
+    print("Agreed. ")
+elif s == "N" or s == "n":
+    print("Not Agreed. ")
+    
--- multiplication.py/user_input1.py
+++ multiplication.py/user_input1.py
@@ -1,5 +1,9 @@
 # Exercise: ask questions; person's name and favourite colour. Then, print out a response on the terminal
-
+'''
 name = input(' what is your name? ')
 favourite_colour = input(' what is your favourite colour? ')
 print(name + ' likes ' + favourite_colour)
+'''
+
+name = 'unwana'
+print(name[1])
